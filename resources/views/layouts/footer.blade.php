5e1cd6a3466d9f19c9ee03de7aa11a4fa51c86b0
carlos <carlosluisamaro@gmil.com>
2023-03-06 01:14:28 -0400

    -avances de ajuste de las dimensiones responsivas en el home

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,34 +2,38 @@
     <div class="container">
         <div class="row covert-list">
             <!-- Footer Location-->
-            <div class="col-lg-4 mb-5 mb-lg-0 list_footer" >
+            <div class="col-lg-4 mb-5 mb-lg-0 " > 
                 <ul style="list-style:none">
-                    <a href="/"><img src="{{asset('assets/img/home/logo_l.jpg')}}"  class="logo_liberfy_footer2"/>
+                    <li>
+                         <a href="/"><img src="{{asset('assets/img/home/logo_l.jpg')}}"  class="logo_liberfy_footer2"/>
+                         <a href="http://www.facebook.com"><i class="fab fa-facebook-f fa-xs socials-2" ></i></a>
+                         <a href="http://www.instagram.com"><i class="fab fa-instagram-square fa-xs socials-3" ></i></a>
+                    </li>
+                </ul>
+                <!-- <a><i class="fab fa-facebook-f"></i></a> -->
+            </div>
+
+            <div class="col-lg-4 mb-5 mb-lg-0 list_footer columna1">
+                <ul style="list-style:none">
+                    <li class="contacto" style="margin-left: 8%;"><a>Contacto</a></li>
+                    <li style="text-align:left"><a class="texto-footer" style="margin-left: 8%;"><i class="far fa-envelope fa-xs"></i>&nbsp;&nbsp;contacto@liberfy.com</a></li>
+                    <li style="text-align:left"><a class="texto-footer" style="margin-left: 8%;"><i class="fa fa-phone fa-xs" ></i>&nbsp;&nbsp;(414) 867 - 5892</a></li>
+                    <li style="text-align:left"><a class="texto-footer" style="margin-left: 8%;"><i class="fa fa-map-marker-alt fa-xs"></i>&nbsp;&nbsp;794 Mcallister St</a></li>
+                    <li style="text-align:left"><a class="texto-footer" style="margin-left: 18%;">San Francisco, 94102</a></li>
                 </ul>
             </div>
 
-            <div class="col-lg-4 mb-5 mb-lg-0 list_footer">
-                <p style="list-style:none">Contacto</p>
-                <!-- <ul style="list-style:none">
-                    <li>Contacto</li>
-                    <li><a href="" style="color:#9A9EA6;text-decoration:none" >Online Chat</a></li>
-                    <li><a href="" style="color:#9A9EA6;text-decoration:none" >Whatsapp</a></li>
-                    <li><a href="" style="color:#9A9EA6;text-decoration:none" >Telegram</a></li>
-                    <li><a href="" style="color:#9A9EA6;text-decoration:none" >Ticketing</a></li>
-                </ul> -->
-            </div>
-
-            <div class="col-lg-4 mb-5 mb-lg-0 list_footer">
-                <ul style="list-style:none">
-                    <li><h5>FAQ</h5></li>
-                    <li><a href="" style="color:#9A9EA6;text-decoration:none" >Account</a></li>
-                    <li><a href="" style="color:#9A9EA6;text-decoration:none" >Manage Deliveries</a></li>
-                    <li><a href="" style="color:#9A9EA6;text-decoration:none" >Orders</a></li>
-                    <li><a href="" style="color:#9A9EA6;text-decoration:none" >Payments</a></li>
-                    <li><a href="" style="color:#9A9EA6;text-decoration:none" >Returns</a></li>
+            <div class="col-lg-4 mb-5 mb-lg-0 list_footer columna2">
+                <ul style="list-style:none ">
+                    <li class="texto-footer" ><a><i class="fa fa-copyright fa-xs"></i>&nbsp;&nbsp;2023 Liberfy</a></li>
+                    <li style="text-align:left"><a class="texto-footer" >Todos los derechos reservados.</a></li>
+                    <li style="text-align:left"><a class="texto-footer" >Políticas de Cookies</a></li>
+                    <li style="text-align:left"><a class="texto-footer" >Políticas de Privacidad</a></li>
+                    <li style="text-align:left"><a class="texto-footer" >Aviso Legal</a></li>
                 </ul>
             </div> 
             <div class="col-lg-12 mb-5 mt-5 mb-lg-0"> </div>
+            
             {{-- Botton Bar logo --}}
             <div class="col-lg-4 mb-5 mb-lg-0">
                <!--  <p class="bottom_bar" style="text-size:12px;color:#9A9EA6;">Botton Bar</p> -->
@@ -74,8 +78,16 @@
                     <i class="fab fa-fw fa-whatsapp" ></i></a>
             </div>
             <!-- Footer About Text-->
-            <div class="col-lg-4 mb-5 mb-lg-0 section_copy">
-                <p class="lead mb-0">
+           
+            <div class="col-lg-4 mb-5 mb-lg-0 ">
+                <hr class="divisor3">
+                <p class="section_copy2">
+                    Copyright &copy; 2023
+                </p>
+                <p class="section_copy4">
+                    All Rights Reserved | <a href="" style="color: #232323;">Terms and Conditions</a> | <a href="" style="color: #232323;">Privacy Policy</a>
+                </p>
+                <p class="lead mb-0 section_copy">
                     <small style="color:#9A9EA6;"> &copy; 2000 - 2022, All Rigths Reserved</small>
                 </p>
             </div>
